46f2022ef79e2b03fdad91503894c40bd0018857
arj <arj>
2024-01-10 16:25:18 +0530

    pro

--- pro/app/models.py
+++ pro/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Shoes(models.Model):
+    brand=models.CharField(max_length=250)
+    gender=models.CharField(max_length=200)
+    category=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
+    price=models.CharField(max_length=200)
+    sale=models.CharField(max_length=10)
+    image=models.ImageField(upload_to='gallery')
+
+    def __str__(self):
+        return self.name
+    
--- pro/app/views.py
+++ pro/app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Shoes
 
 def index(request):
-    return render(request,'index.html')
+    content=Shoes.objects.all()
+    data={
+        'result':content
+    }
+    return render(request,'index.html',data)
 def men(request):
     return render(request,'men.html')
